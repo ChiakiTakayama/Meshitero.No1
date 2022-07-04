dccc3b0b44d7f10121e828df0da8beeabf6f65ce
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-07-04 12:02:08 +0000

    [update]6章ログアウト実装まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # 6章で以下を追加
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # ここまで。この後はrails sでサインアップ画面で動作確認
 end
