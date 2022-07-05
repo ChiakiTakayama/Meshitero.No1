f656a383308cc15c2b34a6b75f361d8b4a4b6bfc
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-07-05 09:05:52 +0000

    [update]Meshitero9章終了まで

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 get'top' => 'homes#top'
+
+resources :post_images, only: [:new, :index, :show]
+# アプリケーションを完成させよう1-1章の「Listsコントローラを作成する」と
+# 〃1-7章のルーティングを簡単に記述するに
+# 〃2-9章のresourcesメソッドのオプション
+
 get "/homes/about" => "homes#about", as: "about"
 end
