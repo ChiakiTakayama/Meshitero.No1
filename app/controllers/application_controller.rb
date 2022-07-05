f5f7032f6bc4966318b74043e5e1501778622713
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-07-05 07:39:17 +0000

    [update]ログイン、ログアウト後にaboutに飛ぶまで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
-  # 6章で以下を追加
+ # 6章で以下を追加
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+ # 7章で以下を追加 
+   def after_sign_in_path_for(resource)
+     about_path
+   end
+  
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+  # ここまで7章で追加したけれどNameエラー
+  # ↑routes.rbの記述が違っていたから。8章に記述
   protected
 
   def configure_permitted_parameters
